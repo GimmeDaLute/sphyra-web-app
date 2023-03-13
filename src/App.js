427f7384ea9f8bcb06df296cf240e1acdfdfeae2
GimmeDaLute <peter.m.edwards@outlook.com>
2023-03-13 12:14:37 +0000

    Simple Tribe pages with Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { Home, NotFound } from './pages';
+import { Home, About, NotFound } from './pages';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { Header, Footer } from './layouts';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
         <Route path="/*" element={<NotFound/>}></Route>
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/pages/Tribes/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Tribes() {
+  return (
+    <div>
+      <h2>Tribes</h2>
+    </div>
+    
+  )
+}
